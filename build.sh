9e6d02e1620d1374fd4917daf93cb4e9d939758e
hyiromori <hyiromori@gmail.com>
2021-03-23 17:55:19 +0900

    Add --lite flag.

--- build.sh
+++ build.sh
@@ -5,10 +5,20 @@ INDEX_FILE_PATH="${THIS_DIR}/src/index.ts"
 DIST_DIR="${THIS_DIR}/dist"
 
 mkdir -p "${DIST_DIR}"
-deno compile \
-     --unstable \
-     --allow-net \
-     --allow-env \
-     --target "x86_64-apple-darwin" \
-     --output "${DIST_DIR}/update-issue_x86_64-apple" \
-     "${INDEX_FILE_PATH}"
+
+function build() {
+  local TARGET="$1"
+  deno compile \
+       --lite \
+       --unstable \
+       --allow-net \
+       --allow-env \
+       --target "${TARGET}" \
+       --output "${DIST_DIR}/update-issue_${TARGET}" \
+       "${INDEX_FILE_PATH}"
+}
+
+build "x86_64-unknown-linux-gnu"
+build "x86_64-pc-windows-msvc"
+build "x86_64-apple-darwin"
+build "aarch64-apple-darwin"
